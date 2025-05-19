2e947c188c6fb0448ecb3c73258d9929f6dbaa40
psehgal2 <psehgal@caltech.edu>
2025-05-19 03:22:54 -0700

    added features

--- data/data_processing.py
+++ data/data_processing.py
@@ -9,8 +9,28 @@ data_base = "/Users/psehgal/Documents/physionet.org/files/mimiciv/3.1/hosp"
 df = pd.read_csv(os.path.join(data_base, "patients.csv.gz"))
 diagnosis_codes = pd.read_csv(os.path.join(data_base, "diagnoses_icd.csv.gz"))
 diagnosis_names = pd.read_csv(os.path.join(data_base, "d_icd_diagnoses.csv.gz"))
+d_hcpcs = pd.read_csv(os.path.join(data_base, "d_hcpcs.csv.gz"))
+hcpcsevents = pd.read_csv(os.path.join(data_base, "hcpcsevents.csv.gz"))
 admissions = pd.read_csv(os.path.join(data_base, "admissions.csv.gz"))
 # print(df[0:6])
+
+
+def get_diagnoses(hadm_id):
+    admission_codes = diagnosis_codes[diagnosis_codes["hadm_id"] == hadm_id]
+    if not admission_codes.empty:
+        admission_codes = admission_codes["icd_code"].dropna().tolist()
+        admission_diagnoses = []
+        for admission_code in admission_codes:
+            diagnosis_name = diagnosis_names[diagnosis_names["icd_code"] == admission_code]
+            if not diagnosis_name.empty:
+                admission_diagnoses.append(diagnosis_name["long_title"].iloc[0])
+    return admission_diagnoses
+
+def get_hcpcsevents(hadm_id):
+    admission_codes = hcpcsevents[hcpcsevents["hadm_id"] == hadm_id]
+    if not admission_codes.empty:
+        icd_codes = admission_codes["icd_code"]
+        # real world events 
 
 count = 0
 
@@ -34,23 +54,27 @@ for idx, patient in df.iterrows():
     # TODO: Remove age should be in admissions table
     patient_info_dict["patient age"] = age
     patient_info_dict["age of death"] = dod_age
-    # print(patient_info_dict)
-    # print(admissions.head())
-    # TODO: improve efficience of this 
+    # TODO: improve efficiency of this 
     patient_admissions = admissions[admissions['subject_id'] == patient_id]
     if not patient_admissions.empty:
         patient_info_dict["race"] = patient_admissions.iloc[0]["race"]
+        # TODO: add relavent information to patient_admissions_dict {hadm_id1: {}, hadm_id2: {}}
         for idx, admission in patient_admissions.iterrows():
-            patient_admissions_dict[admission["hadm_id"]] = {}
-            # TODO: add relavent information to patient_admissions_dict {hadm_id1: {}, hadm_id2: {}}
+            hadm_id = admission["hadm_id"]
+            patient_admissions_dict[hadm_id] = {}
+            admission_diagnoses = get_diagnoses(hadm_id)
+            if admission_diagnoses:
+                patient_admissions_dict[hadm_id]["diagnoses"] = admission_diagnoses
+            else:
+                pass
+                # TODO: need to handle this?
 
+        # diagnosis_codes
     else:
         patient_info_dict["race"] = "Unknown"
-    # print(patient_admissions)
+    # print(hcpcsevents)
     print(patient_info_dict)
     print(patient_admissions_dict)
-
-
     break
 
 
